924d2cc5f7d056e3aba1ff8cdf5dd76d0a709d2d
Karibash <y.karibayashi@gmail.com>
2025-09-10 11:20:09 +0900

    chore: Add a llms copy buttons

--- website/theme/index.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import-x/export */
-
-export * from '@rspress/core/theme';
-
-export { HomeLayout } from './layouts/home-layout';
---
+++ website/theme/index.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable import-x/export */
+import { getCustomMDXComponent as baseGetCustomMDXComponent } from '@rspress/core/theme';
+import { LlmsContainer, LlmsCopyButton, LlmsViewOptions } from '@rspress/plugin-llms/runtime';
+
+export * from '@rspress/core/theme';
+
+export { HomeLayout } from './layouts/home-layout';
+
+export const getCustomMDXComponent = (): ReturnType<typeof baseGetCustomMDXComponent> => {
+  const { h1: H1, ...components } = baseGetCustomMDXComponent();
+  return {
+    ...components,
+    h1: (props) => {
+      return (
+        <>
+          <H1 {...props} />
+          <LlmsContainer>
+            <LlmsCopyButton />
+            <LlmsViewOptions />
+          </LlmsContainer>
+        </>
+      );
+    },
+  };
+};
