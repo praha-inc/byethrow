017d5c09c2a4434f64f76d30d62c73e201f04b50
Karibash <y.karibayashi@gmail.com>
2025-11-17 10:57:22 +0900

    chore: Polish the style of the home layout

--- website/theme/layouts/home-layout/home-layout.tsx
+++ website/theme/layouts/home-layout/home-layout.tsx
@@ -6,7 +6,7 @@ export const HomeLayout: FC = () => {
   return (
     <BaseHomeLayout
       afterHeroActions={(
-        <div className="rp-doc" style={{ width: '100%', maxWidth: 400 }}>
+        <div className="rp-doc" style={{ width: '100%', maxWidth: 450, margin: '-1rem 0' }}>
           <PackageManagerTabs command="install @praha/byethrow" />
         </div>
       )}
