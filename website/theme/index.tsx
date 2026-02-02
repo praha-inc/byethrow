8ffaaa60cfa05de442d9f8f1dfedc789cd64e3fc
Karibash <y.karibayashi@gmail.com>
2026-02-02 23:27:10 +0900

    chore: Remove llms plugin

--- website/theme/index.tsx
+++ website/theme/index.tsx
@@ -1,26 +1,5 @@
 /* eslint-disable import-x/export */
-import { getCustomMDXComponent as baseGetCustomMDXComponent } from '@rspress/core/theme-original';
-import { LlmsContainer, LlmsCopyButton, LlmsViewOptions } from '@rspress/plugin-llms/runtime';
-
 export * from '@rspress/core/theme-original';
 
 export * from './components/stackblitz';
 export { HomeLayout } from './layouts/home-layout';
-
-export const getCustomMDXComponent = (): ReturnType<typeof baseGetCustomMDXComponent> => {
-  const { h1: H1, ...components } = baseGetCustomMDXComponent();
-  return {
-    ...components,
-    h1: (props) => {
-      return (
-        <>
-          <H1 {...props} />
-          <LlmsContainer>
-            <LlmsCopyButton />
-            <LlmsViewOptions />
-          </LlmsContainer>
-        </>
-      );
-    },
-  };
-};
